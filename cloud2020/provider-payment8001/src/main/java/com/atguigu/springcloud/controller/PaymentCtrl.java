2ad915ffaf10ec935b5950bf4496521f6518c727
DESKTOP-RS4GUET\Administrator <wangzongbao@seaskysh.com>
2020-11-05 17:36:18 +0800

    关闭Eureka自我保护

--- cloud2020/provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentCtrl.java
+++ cloud2020/provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentCtrl.java
@@ -5,12 +5,17 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @PackageName:com.atguigu.springcloud.controller
@@ -23,9 +28,31 @@ import javax.annotation.Resource;
 @Api(tags = "支付Ctrl")
 @RestController
 @RequestMapping("PaymentCtrl")
+@Slf4j
 public class PaymentCtrl {
     @Resource
     PaymentService paymentService;
+
+    @Value("${server.port}")  //从application配置文件读取server.port的value
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @ApiOperation(value = "1. 服务发现")
+    @PostMapping("discovery")
+    public Object discovery(){
+        //所有微服务
+        List<String> services = discoveryClient.getServices();
+        log.info(services.toString());
+        //一个微服务下的所有实例（集群）
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        //return services;
+        for (ServiceInstance instance:instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
 
     @ApiOperation(value = "1. 新增编辑")
     @PostMapping("createOrUpdate")
