0251515f7dc0747be13a91e02bb2345a466eab66
王宗保 <1304068397@qq.com>
2021-04-06 15:34:07 +0800

    add: hystrix

---
+++ cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/apringcloud/service/PaymentService.java
@@ -0,0 +1,32 @@
+package com.atguigu.apringcloud.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+
+    //成功
+    public String paymentInfo_OK(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_OK,id：  "+id+"\t"+"哈哈哈"  ;
+    }
+
+    //失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",
+           commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")  //3秒钟以内就是正常的业务逻辑
+           })
+    public String paymentInfo_TimeOut(Integer id){
+        int timeNumber = 5;
+        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "服务降级啦，线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜";
+    }
+
+}
+
