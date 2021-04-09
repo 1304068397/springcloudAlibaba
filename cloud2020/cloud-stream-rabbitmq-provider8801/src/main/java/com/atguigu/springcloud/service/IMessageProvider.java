80b9040302023c3be2719fa2cb104baf6a620d4a
王宗保 <1304068397@qq.com>
2021-04-09 15:07:53 +0800

    add: springcloud stream

---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/IMessageProvider.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud.service;
+
+/**
+ * @PackageName:com.atguigu.springcloud.service
+ * @ClassName:IMessageProvider
+ * @Description:
+ * @Author:王宗保
+ * @Version V1.0.0
+ * @Date:2021/4/9 13:51
+ */
+public interface  IMessageProvider {
+
+    public String send();
+
+}
